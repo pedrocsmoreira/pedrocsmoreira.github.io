5ed2e920259cbc11a8d33966f30efc8f3b71fab9
pedrocsmoreira <81022217+pedrocsmoreira@users.noreply.github.com>
2022-11-22 09:14:58 +0000

    updates

--- components/Projects.vue
+++ components/Projects.vue
@@ -1,29 +1,51 @@
-<template>
-    <div id="products">
-        <div class="productsItemContainer" v-for="product in products">
-            <div class="productsItem">
-        
-            </div>
+<template id="card-component">
+    <div class="card text-dark card-space">
+        <img v-bind:src="`${item.icon}`" class="card-img-top" alt="projectIcon">
+        <div class="card-body">
+            <h5 class="card-title">{{ item.name }}</h5>
+            <p class="card-text text-justify">{{ item.description }}</p>
+            <a v-bind:href="`${item.link}`" class="btn btn-secondary">See more...</a>
         </div>
     </div>
 </template>
-  
-<script>
-    export default {
-        name: 'Projects',
 
-        data() {
-            return {
-                products: []
-            }
-        },
+<script type="module">
+    import { createApp } from 'https://unpkg.com/petite-vue?module'
 
-        mounted() {
-            axios
-                .get("./assets/projects.json")
-                .then(response => {
-                    this.projects = response.data.projects;
-                });
+    function projects(){
+        return {
+            items: [{
+                    "icon": "./assets/black-transparent-logo.png",
+                    "name": "project1",
+                    "description": "description of project one",
+                    "link": "./components/projects.html"
+                },
+                {
+                    "icon": "./assets/black-transparent-logo.png",
+                    "name": "project2",
+                    "description": "description of project two",
+                    "link": "./components/projects.html"
+                },
+                {
+                    "icon": "./assets/black-transparent-logo.png",
+                    "name": "project3",
+                    "description": "description of project three",
+                    "link": "./components/projects.html"
+                }
+            ]
         }
     }
+
+
+    function cardBuilder(props){
+        return{
+            $template: '#card-component',
+            icon: props.icon,
+            name: props.name,
+            description: props.description,
+            link: props.link
+        }
+    }
+
+    createApp({projects, cardBuilder}).mount()
 </script>
