31a2d5f2919eff610e304232fae91633b7fcd104
pedrocsmoreira <81022217+pedrocsmoreira@users.noreply.github.com>
2022-11-22 09:06:08 +0000

    new version

---
+++ components/Projects.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="products">
+        <div class="productsItemContainer" v-for="product in products">
+            <div class="productsItem">
+        
+            </div>
+        </div>
+    </div>
+</template>
+  
+<script>
+    export default {
+        name: 'Projects',
+
+        data() {
+            return {
+                products: []
+            }
+        },
+
+        mounted() {
+            axios
+                .get("./assets/projects.json")
+                .then(response => {
+                    this.projects = response.data.projects;
+                });
+        }
+    }
+</script>
